f9580ef7653fce6b01249d12565f648a372a3e59
kapilbaghel <kapilbaghelabc27@gmail.com>
2026-06-02 16:59:40 +0530

    Add performance logs

--- src/app/api/trains/route.ts
+++ src/app/api/trains/route.ts
@@ -14,12 +14,16 @@ export async function GET(req: NextRequest) {
 
     console.log("API HIT");
 
-    const res = await fetch(
-      `https://travel.paytm.com/api/trains/v5/search?departureDate=${date}&destination=${to}&dimension114=seo-home&isAscOfferEligible=false&isH5=true&is_new_user=null&quota=GN&show_empty=true&source=${from}&client=web&deviceIdentifier=Mozilla%20Firefox-147.0.0.0`,
-      {
-        cache: "no-store",
-      }
-    );
+    console.time("paytm-api");
+
+const res = await fetch(
+  `https://travel.paytm.com/api/trains/v5/search?departureDate=${date}&destination=${to}&dimension114=seo-home&isAscOfferEligible=false&isH5=true&is_new_user=null&quota=GN&show_empty=true&source=${from}&client=web&deviceIdentifier=Mozilla%20Firefox-147.0.0.0`,
+  {
+    cache: "no-store",
+  }
+);
+
+console.timeEnd("paytm-api");
 
     if (!res.ok) {
       throw new Error(`Paytm API Error: ${res.status}`);
