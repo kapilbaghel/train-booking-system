cf76a3e2ab6b74e33e500861310aed7582a9e222
kapilbaghel <kapilbaghelabc27@gmail.com>
2026-06-11 18:59:51 +0530

    did some changes in UI and add passenger details page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
-"use client"
+"use client";
+
+import { Suspense } from "react";
 import StationSearch from "@/components/StationSearch";
 import Navbar from "@/components/Navbar";
 import InfoCard from "@/components/Herocard";
 import PopularRoutes from "@/components/PopularRoutes";
 import Footer from "@/components/Footer";
 
-export default function Home(){
-
-
-  return(
+export default function Home() {
+  return (
     <div className="bg-black min-h-screen overflow-x-hidden">
-
-      <Navbar/>
-      <InfoCard/>
+      <Navbar />
+      <InfoCard />
 
       <div id="station-search">
-      <StationSearch/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <StationSearch />
+        </Suspense>
       </div>
 
-      <PopularRoutes/>
-      <Footer/>
+      <PopularRoutes />
+      <Footer />
     </div>
-  )
+  );
 }
